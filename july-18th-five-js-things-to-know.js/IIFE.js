e2f063f75a74f68c6af59288fd6313b5a877370a
loganmulvihill <mulvihill.logan@gmail.com>
2020-07-18 13:22:18 -0500

    learned about IIFE or immediately invoked function expression and why they are useful

---
+++ july-18th-five-js-things-to-know.js/IIFE.js
@@ -0,0 +1,9 @@
+//Immediataely Invoked Function Expression (IIFE)
+
+(fucntion(){
+  console.log('heyyy');
+})();
+
+console.log('final');
+
+//You would use this for data privacy, many libraries do this.
