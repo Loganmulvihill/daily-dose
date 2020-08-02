96ca32130c3309f92c80c067912fc3bf918d693b
loganmulvihill <mulvihill.logan@gmail.com>
2020-08-01 23:51:41 -0500

    added header type and added body with value JSON.stringify method

--- august-1-fetch-api/script.js
+++ august-1-fetch-api/script.js
@@ -1,10 +1,22 @@
-fetch('https://reqres.in/api/users/')
+fetch('https://reqres.in/api/users/', {
+      method: 'Post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: 'User 1'
+      })
+})
   .then( res => {
-    if(res.ok) {
-      console.log('success')
-    } else {
-      console.log("Not Successful")
-    }
+
+    return res.json();
+    //Below is an example of an if else conditional you can implement to handle a 404 error
+
+    // if(res.ok) {
+    //   console.log('success')
+    // } else {
+    //   console.log("Not Successful")
+    // }
   })
   .then(data => console.log(data))
   .catch(error => console.log('error'))
